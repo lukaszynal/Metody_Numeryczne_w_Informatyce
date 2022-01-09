196d1ce0021ce813944b83eda8a72f5433c04e41
Łukasz <81504969+lukaszynal@users.noreply.github.com>
2022-01-09 12:10:15 +0100

    Update and rename MNwI_interpolacjaOrazCalkaMetodaTrapezow.cpp to MNwI_interpolacja_calkaMetodaTrapezow.cpp

--- MNwI_interpolacja_calkaMetodaTrapezow.cpp
+++ MNwI_interpolacja_calkaMetodaTrapezow.cpp
@@ -1,4 +1,4 @@
-// Program: MNwI_interpolacjaOrazCalkaMetodaTrapezow, 11.11.2021
+// Program: MNwI_interpolacja_calkaMetodaTrapezow, 11.11.2021
 // Autor: Szynal Lukasz, 150063, 2021/2022, Informatyka, D2, II semestr
 
 /*
